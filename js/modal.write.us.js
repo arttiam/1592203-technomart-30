2b075f9206046c4e31c8614ac9b28023cd5ed71b
arttiam <71148141+arttiam@users.noreply.github.com>
2021-02-24 01:35:48 +0500

    правки

--- js/modal.write.us.js
+++ js/modal.write.us.js
@@ -1,17 +1,61 @@
-const modal = document.querySelector(".modal-write-us");
-const linkContacts = document.querySelector(".link-contacts");
-const closeModal = modal.querySelector(".btn-close-modal");
-const inputName = modal.querySelector(".input-name");
-const inputEmail = modal.querySelector(".input-email");
-const textModal = modal.querySelector(".text-mail-modal");
+let modalWrite = document.querySelector(".modal-write-us");
+const linkModalWrite = document.querySelector(".link-contacts");
+const btnSubmit = document.querySelector(".style-btn-send-modal");
+const btnClose = document.querySelector(".btn-modal-write-us");
+const inputName = document.querySelector(".input-name");
+const inputEmail = document.querySelector(".input-email");
+const textMail = document.querySelector(".text-mail-modal");
 
-
-linkContacts.addEventListener("click", function(evt) {
+linkModalWrite.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modal.classList.remove("hiding-content");
+  modalWrite.classList.remove("not-filled");
+  inputEmail.classList.remove('not-filled-shadow');
+  inputName.classList.remove('not-filled-shadow');
+  textMail.classList.remove('not-filled-shadow');
+  modalWrite.classList.remove("hiding-content");
 });
 
-closeModal.addEventListener("click", function() {
-    modal.classList.add("hiding-content");
+btnClose.addEventListener("click", function() {
+  modalWrite.classList.add("hiding-content");
 });
 
+window.addEventListener("keydown", function(evt){
+  if (evt.keyCode === 27) {
+    modalWrite.classList.add("hiding-content");
+  };
+});
+
+btnSubmit.addEventListener("click", function(evt) {
+  if ((inputEmail.value === "") || (inputName === "")  || (textMail === "")) {
+    evt.preventDefault();
+
+    if (inputEmail.value === "") {
+      inputEmail.classList.add('not-filled-shadow');
+    };
+    if (inputName.value === "") {
+      inputName.classList.add('not-filled-shadow');
+    };
+    if (textMail.value === "") {
+      textMail.classList.add('not-filled-shadow');
+    };
+
+    if (modalWrite.classList.contains("not-filled")) {
+      modalWrite.classList.remove("not-filled");
+      modalWrite.classList.add("hiding-content");
+      modalWrite.classList.remove("hiding-content");
+    };
+      modalWrite.classList.add("not-filled");
+  };
+});
+
+inputName.addEventListener("focus", function() {
+  inputName.classList.remove("not-filled-shadow");
+});
+
+inputEmail.addEventListener("focus", function() {
+  inputEmail.classList.remove("not-filled-shadow");
+});
+
+textMail.addEventListener("focus", function() {
+  textMail.classList.remove("not-filled-shadow");
+});
