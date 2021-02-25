ff7a7763859df18e113dc47c8d6628329cd6be09
arttiam <71148141+arttiam@users.noreply.github.com>
2021-02-26 00:40:54 +0500

    правка

--- js/modal.js
+++ js/modal.js
@@ -33,104 +33,124 @@ buyClose.addEventListener("click", function(evt) {
   modalBuy.classList.add("visually-hidden");
 });
 
-linkModalWrite.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modalWrite.classList.remove("hiding-content");
-  modalWrite.classList.add("opening-modal");
-  inputName.focus();
-  if (storageName) {
-    inputName.value = storageName;
-  } else {
-    inputName.value = "";
-  };
-  if (storageEmail) {
-    inputEmail.value = storageEmail;
-  } else {
-    inputEmail.value = "";
-  };
-  textMail.value = "";
-});
-
-btnClose.addEventListener("click", function() {
-  modalWrite.classList.remove("not-filled");
-  inputEmail.classList.remove('not-filled-shadow');
-  inputName.classList.remove('not-filled-shadow');
-  textMail.classList.remove('not-filled-shadow');
-  modalWrite.classList.add("hiding-content");
-});
-
 window.addEventListener("keydown", function(evt){
   if (evt.keyCode === 27) {
-    if (!modalWrite.classList.contains("hiding-content")) {
-      modalWrite.classList.remove("not-filled");
-      inputEmail.classList.remove('not-filled-shadow');
-      inputName.classList.remove('not-filled-shadow');
-      textMail.classList.remove('not-filled-shadow');
-      modalWrite.classList.add("hiding-content");
-    };
-    if (!modalMap.classList.contains("visually-hidden")) {
-      modalMap.classList.add("visually-hidden");
+    if (!modalBuy.classList.contains("visually-hidden")) {
+      modalBuy.classList.add("visually-hidden");
     };
   };
 });
 
-btnSubmit.addEventListener("click", function(evt) {
-  if ((inputEmail.value === "") || (inputName.value === "")  || (textMail.value === "")) {
+if (document.querySelector(".modal-write-us")) {
+
+  linkModalWrite.addEventListener("click", function (evt) {
     evt.preventDefault();
-
-    if (inputEmail.value === "") {
-      inputEmail.classList.add('not-filled-shadow');
+    modalWrite.classList.remove("hiding-content");
+    modalWrite.classList.add("opening-modal");
+    inputName.focus();
+    if (storageName) {
+      inputName.value = storageName;
+    } else {
+      inputName.value = "";
     };
-    if (inputName.value === "") {
-      inputName.classList.add('not-filled-shadow');
+    if (storageEmail) {
+      inputEmail.value = storageEmail;
+    } else {
+      inputEmail.value = "";
     };
-    if (textMail.value === "") {
-      textMail.classList.add('not-filled-shadow');
+    textMail.value = "";
+  });
+
+  btnClose.addEventListener("click", function() {
+    modalWrite.classList.remove("not-filled");
+    inputEmail.classList.remove('not-filled-shadow');
+    inputName.classList.remove('not-filled-shadow');
+    textMail.classList.remove('not-filled-shadow');
+    modalWrite.classList.add("hiding-content");
+  });
+
+  btnSubmit.addEventListener("click", function(evt) {
+    if ((inputEmail.value === "") || (inputName.value === "")  || (textMail.value === "")) {
+      evt.preventDefault();
+  
+      if (inputEmail.value === "") {
+        inputEmail.classList.add('not-filled-shadow');
+      };
+      if (inputName.value === "") {
+        inputName.classList.add('not-filled-shadow');
+      };
+      if (textMail.value === "") {
+        textMail.classList.add('not-filled-shadow');
+      };
+  
+      if (modalWrite.classList.contains("not-filled")) {
+        modalWrite.classList.remove("not-filled");
+      };
+      modalWrite.classList.remove("opening-modal");
+      void modalWrite.offsetWidth;
+      modalWrite.classList.add("not-filled");
+    } else {
+      try {
+        storage = localStorage("name", inputName.value);
+      } catch (err) { 
+        isStorageSupport = false;  
+      };
+      if (isStorageSupport) {
+        localStorage("email", inputEmail.value);
+      };
     };
-
-    if (modalWrite.classList.contains("not-filled")) {
-      modalWrite.classList.remove("not-filled");
+  });
+  window.addEventListener("keydown", function(evt){
+    if (evt.keyCode === 27) {
+      if (!modalWrite.classList.contains("hiding-content")) {
+        modalWrite.classList.remove("not-filled");
+        inputEmail.classList.remove('not-filled-shadow');
+        inputName.classList.remove('not-filled-shadow');
+        textMail.classList.remove('not-filled-shadow');
+        modalWrite.classList.add("hiding-content");
+      };
     };
-    modalWrite.classList.remove("opening-modal");
-    void modalWrite.offsetWidth;
-    modalWrite.classList.add("not-filled");
-  } else {
-    try {
-      storage = localStorage("name", inputName.value);
-    } catch (err) { 
-      isStorageSupport = false;  
+  });
+
+  inputName.addEventListener("focus", function() {
+    inputName.classList.remove("not-filled-shadow");
+    textMail.classList.remove("not-filled-shadow");
+    inputEmail.classList.remove("not-filled-shadow");
+  });
+  
+  inputEmail.addEventListener("focus", function() {
+    inputEmail.classList.remove("not-filled-shadow");
+    textMail.classList.remove("not-filled-shadow");
+    inputName.classList.remove("not-filled-shadow");
+  });
+  
+  textMail.addEventListener("focus", function() {
+    textMail.classList.remove("not-filled-shadow");
+    inputName.classList.remove("not-filled-shadow");
+    inputEmail.classList.remove("not-filled-shadow");
+  });
+};
+
+if (document.querySelector(".modal-map")) {
+
+  linkModalMap.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalMap.classList.remove("visually-hidden");
+    modalMap.classList.add("opening-modal-map");
+  });
+
+  btnCloseMap.addEventListener("click", function() {
+    modalMap.classList.add("visually-hidden");
+    modalMap.classList.remove("opening-modal-map");
+  });
+
+  window.addEventListener("keydown", function(evt){
+    if (evt.keyCode === 27) {
+      if (!modalMap.classList.contains("visually-hidden")) {
+        modalMap.classList.add("visually-hidden");
+        modalMap.classList.remove("opening-modal-map");
+      };
     };
-    if (isStorageSupport) {
-      localStorage("email", inputEmail.value);
-    };
-  };
-});
+  });
 
-inputName.addEventListener("focus", function() {
-  inputName.classList.remove("not-filled-shadow");
-  textMail.classList.remove("not-filled-shadow");
-  inputEmail.classList.remove("not-filled-shadow");
-});
-
-inputEmail.addEventListener("focus", function() {
-  inputEmail.classList.remove("not-filled-shadow");
-  textMail.classList.remove("not-filled-shadow");
-  inputName.classList.remove("not-filled-shadow");
-});
-
-textMail.addEventListener("focus", function() {
-  textMail.classList.remove("not-filled-shadow");
-  inputName.classList.remove("not-filled-shadow");
-  inputEmail.classList.remove("not-filled-shadow");
-});
-
-linkModalMap.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalMap.classList.remove("visually-hidden");
-  modalMap.classList.add("opening-modal-map");
-});
-
-btnCloseMap.addEventListener("click", function() {
-  modalMap.classList.add("visually-hidden");
-  modalMap.classList.remove("opening-modal-map");
-});
+};
--- js/stock.js
+++
@@ -1,9 +0,0 @@
-let products = document.querySelectorAll(".product-item");
-
-for (product of products) {
- if (!product.querySelector(".old-price")) {
-    if (!product.querySelector(".caption-product").classList.contains("product-no-stock")) {
-      product.querySelector(".caption-product").classList.add("product-no-stock");
-    };
- };
-};
